27aa514c50f7f005540fe22ffb03ee6007f7e1bb
David McClure <david.mcclure@virginia.edu>
2012-09-19 11:53:43 -0400

    circle and mean_center functions

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import math
 
+
 def circle(count):
 
     '''Generate the coordinate positions for a circle
@@ -17,9 +18,33 @@ def circle(count):
 
     # Walk the word count.
     for i in range(count):
-        angle = interval * i
+        angle = (interval * i) * (math.pi / 180)
         x = radius * math.cos(angle)
         y = radius * math.sin(angle)
         points.append((x, y))
 
     return points
+
+
+def mean_center(points):
+
+    '''Compute the mean center of a set of points.
+
+    :param list points: A list of x/y tuples.
+
+    :return tuple center: The mean center points.'''
+
+    xs = [x for x,y in points]
+    ys = [y for x,y in points]
+    return (avg(xs), avg(ys))
+
+
+def avg(numbers):
+
+    '''Average a list.
+
+    :param list numbers: List of numbers.
+
+    :return float: The average.'''
+
+    return float(sum(numbers)) / len(numbers)
