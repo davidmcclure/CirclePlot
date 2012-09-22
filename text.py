bd52e13a4a04797fa2bfabb717a3ece5617b1ac7
David McClure <david.mcclure@virginia.edu>
2012-09-21 23:00:34 -0400

    scrub

--- text.py
+++ text.py
@@ -25,15 +25,3 @@ class Text:
 
         self.tokens = nltk.word_tokenize(self.text)
         self.vocab = set(self.tokens)
-
-
-    def zipper(self, length):
-
-        '''Yield heads and tails of segments.
-
-        :param int length: The segment length.
-
-        :yield tuple: The head and tail.'''
-
-        for i in range(1, len(self.tokens) - length):
-            yield (self.tokens[i], self.tokens[i+length])
--- util.py
+++ util.py
@@ -19,66 +19,8 @@ def generate_circle(count):
     # Walk the word count.
     for i in range(count):
         angle = (interval * i) * (math.pi / 180)
-        # x = radius * math.cos(angle)
-        # y = radius * math.sin(angle)
-        x = math.cos(angle)
-        y = math.sin(angle)
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
         points.append((x, y))
 
     return points
-
-
-def mean_center(points):
-
-    '''Compute the mean center of a set of points.
-
-    :param list points: A list of x/y tuples.
-
-    :return tuple center: The mean center points.'''
-
-    xs = [x for x,y in points]
-    ys = [y for x,y in points]
-    return (avg(xs), avg(ys))
-
-
-def sum(points):
-
-    '''Compute the sum of a set of vectors.
-
-    :param list vectors: A list of x/y tuples.
-
-    :return tuple sum: The sum.'''
-
-    xs = [x for x,y in points]
-    ys = [y for x,y in points]
-    return (sum(xs), sum(ys))
-
-
-def markers(length, ticks):
-
-    '''Generate a list of offset markers on a token list
-    with format [(X%, offset), ...].
-
-    :param int length: The number of tokens.
-    :param int ticks: The number of partitions.
-
-    :return list partitions: The partitions.'''
-
-    partitions = []
-
-    for i in range(1, ticks):
-        index = int((1/float(ticks)) * i * length)
-        partitions.append((str(i), index))
-
-    return partitions
-
-
-def avg(numbers):
-
-    '''Average a list.
-
-    :param list numbers: List of numbers.
-
-    :return float: The average.'''
-
-    return float(sum(numbers)) / len(numbers)
